36bf7dad3138f3345f9fd657db5fdcbfa86cc4a8
Peter Mills <peter.mills@bedegaming.com>
2019-07-04 17:21:24 +0100

    Removed some params that are unlikely to be changed

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,12 +1,9 @@
 class nexus_exporter (
-  $exporter_port         = $nexus_exporter::params::exporter_port,
-  $namespace             = $nexus_exporter::params::namespace,                 
+  $exporter_port         = $nexus_exporter::params::exporter_port,          
   $log_level             = $nexus_exporter::params::log_level,
-  $host                  = $nexus_exporter::params::host,
   $nexus_user            = $nexus_exporter::params::nexus_user,
   $nexus_user_password   = $nexus_exporter::params::nexus_user_password,
   $json_data_url         = $nexus_exporter::params::json_data_url,
-  $metric_name_prefix    = $nexus_exporter::params::metric_name_prefix,
   $script_dir            = $nexus_exporter::params::script_dir,
   $service_ensure        = $nexus_exporter::service_ensure,
   $service_enable        = $nexus_exporter::service_enable,
--- manifests/params.pp
+++ manifests/params.pp
@@ -11,12 +11,9 @@ class nexus_exporter::params {
   $service_file          = '/usr/lib/systemd/system/nexus_exporter.service' 
 
 #Exporter Script Config
-  $exporter_port         = 9184 
-  $namespace             = 'nexus'              
+  $exporter_port         = 9184           
   $log_level             = 'info'
-  $host                  = 'localhost'
   $nexus_user            = 'metrics_exporter'
   $nexus_user_password   = 'metrics'
   $json_data_url         = 'http://127.0.0.1:8081/service/metrics/data'
-  $metric_name_prefix    = 'nexus'
 }
