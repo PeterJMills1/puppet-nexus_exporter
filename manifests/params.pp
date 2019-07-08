ec68c29810c5f6f8bddbb1b360ca2997f4aacae5
Peter Mills <peter.mills@bedegaming.com>
2019-07-08 12:21:04 +0100

    Updated params.pp

--- manifests/params.pp
+++ manifests/params.pp
@@ -13,6 +13,6 @@ class nexus_exporter::params {
   $exporter_port         = 9184           
   $log_level             = 'info'
   $nexus_user            = 'metrics_exporter'
-  $nexus_user_password   = undef
+  $nexus_user_password   = 'metrics'
   $json_data_url         = 'http://127.0.0.1:8081/service/metrics/data'
 }
